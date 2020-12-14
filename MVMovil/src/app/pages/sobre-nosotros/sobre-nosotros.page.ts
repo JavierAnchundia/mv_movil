94e047a6cec637d76104748a1f985a567effb92d
bryantutiven2 <bryan.tutiven.2@outlook.com>
2020-12-14 17:10:05 -0500

    Se agrego funcionalidad para que si la app no tiene internet se muestre información de nosotros (del camposanto) guardada en la app

--- MVMovil/src/app/pages/sobre-nosotros/sobre-nosotros.page.ts
+++ MVMovil/src/app/pages/sobre-nosotros/sobre-nosotros.page.ts
@@ -1,11 +1,11 @@
 import { Component, OnInit } from "@angular/core";
 import { Plugins } from "@capacitor/core";
-import { CamposantoService } from 'src/app/services/camposanto/camposanto.service';
-import { GeolocalizacionService } from 'src/app/services/geolocalizacion/geolocalizacion.service';
+import { CamposantoService } from "src/app/services/camposanto/camposanto.service";
+import { GeolocalizacionService } from "src/app/services/geolocalizacion/geolocalizacion.service";
 import { RedSocialService } from "src/app/services/red-social/red-social.service";
-import URL_SERVICIOS from 'src/app/config/config';
-import { environment } from 'src/environments/environment';
-const { App } = Plugins;
+import URL_SERVICIOS from "src/app/config/config";
+import { environment } from "src/environments/environment";
+const { App, Network } = Plugins;
 
 @Component({
   selector: "app-sobre-nosotros",
@@ -13,16 +13,20 @@ const { App } = Plugins;
   styleUrls: ["./sobre-nosotros.page.scss"],
 })
 export class SobreNosotrosPage implements OnInit {
-  latCamposanto: number = -2.0508396;
-  lngCamposanto: number = -79.8998371;
+  latCamposanto: number;
+  lngCamposanto: number;
   linkFacebook: string = "";
+  linkInstagram: string = "";
+  linkTwitter: string = "";
   nombre: string = "";
   direccion: string = "";
   telefono: string = "";
   linkPagina: string = "";
   correo: string = "";
   imagen: string = "";
-  url_backend: String = URL_SERVICIOS.url_backend;
+  url_backend: string = URL_SERVICIOS.url_backend;
+  url_image: string;
+
   constructor(
     private _camposanto: CamposantoService,
     private _geolocalizacion: GeolocalizacionService,
@@ -30,52 +34,68 @@ export class SobreNosotrosPage implements OnInit {
   ) {}
 
   ngOnInit() {
-    let idCamposanto = environment.camposanto.idCamposanto;
-    this.loadCamposanto(idCamposanto);
-    this.loadGeolocalizacion(idCamposanto);
-    this.loadRedSocial(idCamposanto);
+    this.network();
   }
 
-  loadGeolocalizacion(id){
-    this._geolocalizacion.getListGeolocalizacion(id).subscribe(
-      (data)=>{
-        this.latCamposanto = data[0]["latitud"];
-        this.lngCamposanto = data[0]["longitud"];
-      }
-    );
+  async network() {
+    let status = await Network.getStatus();
+    console.log(status.connected);
+    if (!status.connected) {
+      this.latCamposanto = environment.camposanto.coordenadas.latitud;
+      this.lngCamposanto = environment.camposanto.coordenadas.longitud;
+      this.linkFacebook = environment.camposanto.redes.linkFacebook;
+      this.nombre = environment.camposanto.nombre;
+      this.direccion = environment.camposanto.direccion;
+      this.telefono = environment.camposanto.telefono;
+      this.linkPagina = environment.camposanto.linkPagina;
+      this.correo = environment.camposanto.correo;
+      this.url_image = environment.camposanto.imagen;
+      this.linkInstagram = environment.camposanto.redes.linkInstagram;
+      this.linkTwitter = environment.camposanto.redes.linkTwitter;
+    } else {
+      let idCamposanto = environment.camposanto.idCamposanto;
+      this.loadCamposanto(idCamposanto);
+      this.loadGeolocalizacion(idCamposanto);
+      this.loadRedSocial(idCamposanto);
+    }
+  }
+  loadGeolocalizacion(id) {
+    this._geolocalizacion.getListGeolocalizacion(id).subscribe((data) => {
+      this.latCamposanto = data[0]["latitud"];
+      this.lngCamposanto = data[0]["longitud"];
+    });
   }
 
-  loadRedSocial(id){
-    this._redSocial.getRedes(id).subscribe(
-      (data: any)=>{
-        for(let red of data){
-          if(red['nombre'] == "facebook"){
-            this.linkFacebook = red['link']
-          }
+  loadRedSocial(id) {
+    this._redSocial.getRedes(id).subscribe((data: any) => {
+      for (let red of data) {
+        if (red["nombre"] == "facebook") {
+          this.linkFacebook = red["link"];
+        } else if (red["nombre"] == "instagram") {
+          this.linkInstagram = red["link"];
+        } else if (red["nombre"] == "twitter") {
+          this.linkTwitter = red["link"];
         }
       }
-    )
+    });
   }
 
-  loadCamposanto(id){
-    this._camposanto.getCamposantoByID(id).subscribe(
-      (data)=>{
-        this.nombre = data["nombre"];
-        this.direccion = data["direccion"];
-        this.telefono = data["telefono"];
-        this.imagen = data["logo"];
-        this.loadEmpresa(data["id_empresa"]);
-      }
-    )
+  loadCamposanto(id) {
+    this._camposanto.getCamposantoByID(id).subscribe((data) => {
+      this.nombre = data["nombre"];
+      this.direccion = data["direccion"];
+      this.telefono = data["telefono"];
+      this.imagen = data["logo"];
+      this.url_image = this.url_backend + this.imagen;
+      this.loadEmpresa(data["id_empresa"]);
+    });
   }
 
-  loadEmpresa(id){
-    this._camposanto.getEmpresa(id).subscribe(
-      (data)=>{
-        this.correo = data["correo"];
-        this.linkPagina = data["web"];
-      }
-    )
+  loadEmpresa(id) {
+    this._camposanto.getEmpresa(id).subscribe((data) => {
+      this.correo = data["correo"];
+      this.linkPagina = data["web"];
+    });
   }
 
   openMap() {
@@ -85,11 +105,35 @@ export class SobreNosotrosPage implements OnInit {
   }
 
   async openFacebook() {
-    await App.canOpenUrl({
-      url: this.linkFacebook,
-    });
-    await App.openUrl({
-      url: this.linkFacebook,
-    });
+    if (this.linkFacebook != "") {
+      await App.canOpenUrl({
+        url: this.linkFacebook,
+      });
+      await App.openUrl({
+        url: this.linkFacebook,
+      });
+    }
+  }
+
+  async openTwitter() {
+    if (this.linkTwitter != "") {
+      await App.canOpenUrl({
+        url: this.linkTwitter,
+      });
+      await App.openUrl({
+        url: this.linkTwitter,
+      });
+    }
+  }
+
+  async openInstagram() {
+    if (this.linkInstagram != "") {
+      await App.canOpenUrl({
+        url: this.linkInstagram,
+      });
+      await App.openUrl({
+        url: this.linkInstagram,
+      });
+    }
   }
 }
--- MVMovil/src/environments/environment.ts
+++ MVMovil/src/environments/environment.ts
@@ -4,7 +4,25 @@
 
 export const environment = {
   api_key: "AIzaSyD4O-t27PSlyGg2_K6fHbPKlO9Tu_x5h-4",
-  camposanto: { idCamposanto: 1, nombre: "", telefono: "593988097982" },
+  camposanto: {
+    idCamposanto: 1,
+    nombre: "Camposanto La Aurora",
+    telefono: "593999482328",
+    correo: "campoecu@parquedelapaz.com",
+    linkPagina: "https://parquedelapaz.com/camposanto/sede/la-aurora",
+    imagen: "assets/FOTO-CAMPOSANTO.png",
+    redes: {
+      linkFacebook: "https://www.facebook.com/parquedelapaz",
+      linkInstagram: "https://www.instagram.com/parquedelapaz/",
+      linkTwitter: "https://twitter.com/parquedelapaz",
+    },
+    direccion:
+      "Av. León Febres Cordero Km. 13 ½ Vía la Puntilla – Pascuales, junto al Colegio Torremar.",
+    coordenadas: {
+      latitud: -2.050996915336444,
+      longitud: -79.89989200230374,
+    },
+  },
   production: false,
   client_id: "tg0UMkMCY2jwCdFT2QwWY9eStMC67py6bCxQJ3gB",
 };
